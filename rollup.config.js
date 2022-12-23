f7891ff3b82378bc4c777ed72722f553346cf2b0
Frank <frank.de.rooij@yoursurprise.com>
2022-12-23 13:46:31 +0100

    Publishing updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 
@@ -32,7 +31,6 @@ export default {
             minimize: true,
             sourceMap: true,
         }),
-        terser(),
     ],
 };
 
