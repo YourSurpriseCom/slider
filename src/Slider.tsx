44e5f0107b7ae164f1012d1b2dc4e9ca6af5c2b1
Frank <Fiz@live.nl>
2023-02-12 09:35:01 +0100

    Improve styling of buttons

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -237,7 +237,11 @@ export const Slider: React.FC<PropsWithChildren> = ({ children }) => {
                 ref={arrowPrevRef}
                 className="slider__button slider__button--prev slider__button--hidden"
             >
-                { '<' }
+                <svg fill="#33333" viewBox="0 0 256 256" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M160,220a11.96287,11.96287,0,0,1-8.48535-3.51465l-80-80a12.00062,12.00062,0,0,1,0-16.9707l80-80a12.0001,12.0001,0,0,1,16.9707,16.9707L96.9707,128l71.51465,71.51465A12,12,0,0,1,160,220Z"></path>
+                    </g>
+                </svg>
             </button>
             <button
                 aria-label="Next slide"
@@ -246,7 +250,11 @@ export const Slider: React.FC<PropsWithChildren> = ({ children }) => {
                 ref={arrowNextRef}
                 className="slider__button slider__button--next slider__button--hidden"
             >
-                { '>' }
+                <svg fill="#333333" viewBox="0 0 256 256" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path d="M96,220a12,12,0,0,1-8.48535-20.48535L159.0293,128,87.51465,56.48535a12.0001,12.0001,0,0,1,16.9707-16.9707l80,80a12.00062,12.00062,0,0,1,0,16.9707l-80,80A11.96287,11.96287,0,0,1,96,220Z"></path>
+                    </g>
+                </svg>
             </button>
         </div>
     );
