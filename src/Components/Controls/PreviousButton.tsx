e48c88bb3820192eb719edec54cc18fb9a6ca6d5
Frank <frank.de.rooij@yoursurprise.com>
2023-03-15 09:06:53 +0100

    Use class name for the icon color, so it can be overwritten

--- src/Components/Controls/PreviousButton.tsx
+++ src/Components/Controls/PreviousButton.tsx
@@ -20,7 +20,7 @@ export const PreviousButton: React.FC<Props> = ({ onClick, isHidden }) => {
                 { 'slider__button--hidden': isHidden },
             ])}
         >
-            <svg fill="#554c44" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 256 256">
+            <svg className="slider__button__icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 256 256">
                 <path d="M160,216a8.5,8.5,0,0,1-5.7-2.3l-80-80a8.1,8.1,0,0,1,0-11.4l80-80a8.1,8.1,0,0,1,11.4,11.4L91.3,128l74.4,74.3a8.1,8.1,0,0,1,0,11.4A8.5,8.5,0,0,1,160,216Z"/>
             </svg>
         </button>
