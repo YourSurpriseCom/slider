da33ac51ec9b33b9dd914f2d25ad5ad1379c68f3
Frank <Fiz@live.nl>
2023-07-28 13:49:33 +0200

    Implement end to end testing

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: './test',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'line',
+    use: {
+        trace: 'on-first-retry',
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ],
+    webServer: {
+        command: 'npm run serve',
+        url: 'http://localhost:1234',
+        reuseExistingServer: !process.env.CI,
+    },
+});
---
+++ test/slider.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+    await page.goto('http://localhost:1234/');
+
+    await expect(page).toHaveTitle('Slider demo');
+});
+
+test('can drag with the mouse to progress the slider', async ({ page }) => {
+    await page.goto('http://localhost:1234/');
+
+    const firstSlider = await page.locator('.slider').first();
+    const slides = await firstSlider.getByRole('listitem');
+    const thirdSlide = slides.nth(2);
+    const firstSlide = slides.first();
+
+    await expect(firstSlide).toBeInViewport();
+
+    await thirdSlide.dragTo(thirdSlide, {
+        force: true,
+        sourcePosition: {
+            y: 100,
+            x: 100,
+        },
+        targetPosition: {
+            x: -300,
+            y: 100,
+        },
+    });
+
+    await expect(firstSlide).not.toBeInViewport();
+    await expect(thirdSlide).toBeInViewport();
+
+    await thirdSlide.dragTo(thirdSlide, {
+        sourcePosition: {
+            y: 100,
+            x: 100,
+        },
+        force: true,
+        targetPosition: {
+            x: 600,
+            y: 100,
+        },
+    });
+
+    await expect(firstSlide).toBeInViewport();
+});
+
+test('can navigate with the buttons', async ({ page }) => {
+    await page.goto('http://localhost:1234/');
+
+    const firstSlider = await page.locator('.slider').first();
+    const slides = await firstSlider.getByRole('listitem');
+
+    const fourthSlide = slides.nth(3);
+    const firstSlide = slides.first();
+
+    const nextSlideButton = await firstSlider.getByLabel('Next slide').first();
+    const prevSlideButton = await firstSlider.getByLabel('Previous slide').first();
+
+    await expect(firstSlide).toBeInViewport();
+
+    await nextSlideButton.click();
+
+    await expect(firstSlide).not.toBeInViewport();
+    await expect(fourthSlide).toBeInViewport();
+
+    await prevSlideButton.click();
+
+    await expect(firstSlide).toBeInViewport();
+});
