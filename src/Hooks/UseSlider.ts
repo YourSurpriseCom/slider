aea777164a19feb3f7a49bdcae67ede5713c7256
Frank de Rooij <Fiz@live.nl>
2026-01-20 10:47:19 +0100

    Improve consistency of return types

--- src/Hooks/UseSlider.ts
+++ src/Hooks/UseSlider.ts
@@ -33,9 +33,13 @@ export const useSlider = (): UseSlider => {
     const getLastVisibleSlideIndex = (): number => visibleSlideIndices.current[visibleSlideIndices.current.length - 1]
         ?? partiallyVisibleSlideIndices.current[partiallyVisibleSlideIndices.current.length - 1] ?? -1;
 
-    const addVisibleSlide = (index: number) => visibleSlideIndices.current.push(index);
+    const addVisibleSlide = (index: number) => {
+        visibleSlideIndices.current.push(index);
+    }
 
-    const addPartiallyVisibleSlide = (index: number) => partiallyVisibleSlideIndices.current.push(index);
+    const addPartiallyVisibleSlide = (index: number) => {
+        partiallyVisibleSlideIndices.current.push(index);
+    }
 
     const removePartiallyVisibleSlide = (index: number) => {
         partiallyVisibleSlideIndices.current = partiallyVisibleSlideIndices.current.filter((slideIndex) => slideIndex !== index);
